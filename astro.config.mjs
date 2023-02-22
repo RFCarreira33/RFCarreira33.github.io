b4072a8a35b0cbf5bfb8fcb85391c8222050294b
Rodrigoporextenso <2213146@my.ipleiria.pt>
2023-02-22 14:47:01 +0000

    Revert "Update astro.config.mjs"
    This reverts commit 35d5f8eb69b6cf8da42e1b6b331d4a371841739d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rfcarreira33.github.io',
+  site: 'https://RFCarreira33.github.io',
   integrations: [tailwind(),]
 });
