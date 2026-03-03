b1918a07a3496674b5a23098db166224d4106bdd
Rodrigo Carreira <rodrigo.carreira@md3studio.com>
2026-03-03 10:37:24 +0000

    About update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,19 +6,16 @@ const About = () => (
     prior coding experience because I wanted to perform a career change.
     <br />
     <br />
-    <h2>Programming languages</h2>
-    I'm currently working with{" "}
-    <a href="https://www.djangoproject.com/">Django</a> and <a>React</a> but
-    I've worked with <b>PHP</b> and the{" "}
-    <a href="https://www.yiiframework.com/"> Yii2</a> framework doing web
-    development, <b>C#</b> and <a href="https://dotnet.microsoft.com/">.Net</a>{" "}
-    creating desktop apps, <b> Java</b> building mobile apps for{" "}
-    <a href="https://developer.android.com/">Android</a> and on my free time I
-    enjoy playing with in <a href="https://www.rust-lang.org/">Rust</a>.
+    <h2>Programming languages</h2>I primarily work with{" "}
+    <a href="https://www.djangoproject.com/">Django</a> and{" "}
+    <a href="https://react.dev/">React</a>. Previously, I've built applications
+    using <b>PHP</b> with <a href="https://laravel.com/">Laravel</a> as the
+    framework, <b>C#</b> and <a href="https://dotnet.microsoft.com/">.Net</a>,
+    and <b>Java</b>. In my free time, I enjoy experimenting with{" "}
+    <a href="https://www.rust-lang.org/">Rust</a>.
     <br />
     <br />
-    <h2>Hardware and Stuff</h2>
-    I have a <b> desktop</b> with a Ryzen 5700x and a
+    <h2>Hardware and Stuff</h2>I have a <b> desktop</b> with a Ryzen 5700x and a
     Radeon 7800xt and my <b> daily driver</b> is a Thinkpad T480s both running{" "}
     <a href="knowyourmeme.com/memes/btw-i-use-arch">Arch</a> as my main{" "}
     <b>OS</b> for development.
